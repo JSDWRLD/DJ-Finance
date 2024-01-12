0964aa47397a3565d4d923e95e73570f833c08df
DJ <123333887+JSDWRLD@users.noreply.github.com>
2024-01-12 12:11:26 -0800

    Dashboard Layout Finished using Grid

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,12 +1,79 @@
-import { Box, useTheme } from '@mui/material';
+import DashboardBox from '@/components/DashboardBox';
+import { Box, useMediaQuery } from '@mui/material';
 
 
 type Props = {}
 
+const gridTemplateLargeScreens = `
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
+`
+
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`
+
 const Dashboard = (props: Props) => {
-  const { palette } = useTheme();
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   return (
-    <Box color={palette.grey[300]}>Dashboard</Box>
+    <Box width="100%" height="100%" display="grid" gap="1.5rem"
+      sx={
+        isAboveMediumScreens ? {
+        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+        gridTemplaterRows: "repeat(10, minmax(60px, 1fr))",
+        gridTemplateAreas: gridTemplateLargeScreens,
+      } : {
+        gridAutoColumns: "1fr",
+        gridAutoRows: "80px",
+        gridTemplateAreas: gridTemplateSmallScreens,
+      }
+    }
+    >
+      
+      <DashboardBox bgcolor="#fff" gridArea="a"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="b"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="d"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="e"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="f"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="g"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="h"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="i"></DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="j"></DashboardBox>
+    </Box>
   )
 }
 
