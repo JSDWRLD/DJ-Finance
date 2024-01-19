d2b0fa21fc89eb111b443c6abe4f028b5a224432
DJ <123333887+JSDWRLD@users.noreply.github.com>
2024-01-19 13:56:51 -0800

    Key performance indicators setup

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
 
 /* CONFIGURATIONS */
 dotenv.config()
@@ -17,6 +18,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/* ROUTES */
+app.use("/kpi", kpiRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
